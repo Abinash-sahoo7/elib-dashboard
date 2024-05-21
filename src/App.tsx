f9dce6d478807301e8bd9f115474feeb952f3a6c
sahooabinash2003@gmail.com <avinashsahoo2021@gmail.com>
2024-05-21 23:29:11 +0530

    Add cadecn Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <>
       <h1>This is E-Lib </h1>
+      <Button variant={"default"}>Click Me</Button>
     </>
   )
 }
