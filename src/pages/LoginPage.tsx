75be322e7621fc51a2abdb8a153b387f4decb1f2
sahooabinash2003@gmail.com <avinashsahoo2021@gmail.com>
2024-05-27 23:08:47 +0530

    persists token-store in local storage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { login } from '@/http/api'
+import useTokenStore from '@/store'
 import { useMutation } from '@tanstack/react-query'
 import { Loader } from 'lucide-react'
 import { useRef } from 'react'
@@ -10,14 +11,16 @@ import { Link, useNavigate } from 'react-router-dom'
 
 function LoginPage() {
 
+  const setToken = useTokenStore((state) => state.setToken) 
   const navigate = useNavigate();
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
   const mutation = useMutation({
     mutationFn: login,
-    onSuccess: () => {
-      console.log("Login successfully");
+    onSuccess: (response) => {
+      // console.log("Login successfully", );
+      setToken(response.data.accessToken);
       navigate('/dashboard/home')
     }
   })
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -2,27 +2,60 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Link } from "react-router-dom"
+import { register } from "@/http/api"
+import { useMutation } from "@tanstack/react-query"
+import { Loader } from "lucide-react"
+import { useRef } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 const RegisterPage = () => {
+
+    const navigate = useNavigate();
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const mutation = useMutation({
+    mutationFn: register,
+    onSuccess: () => {
+      console.log("Register successfully");
+      navigate('/auth/login')
+    }
+  })
+
+  const handleRegisterLogin = () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    const name = nameRef.current?.value;
+    console.log('data', {name, email, password});
+
+    if(!name || !email || !password){
+      return alert('please enter Name, email or password');
+    }
+    // Mutation
+    mutation.mutate({name, email, password})
+  }
+
   return (
     <section className="flex justify-center items-center h-screen">
         <Card className="w-full max-w-sm">
             <CardHeader>
                 <CardTitle className="text-xl">Sign Up</CardTitle>
                 <CardDescription>
-                Enter your information to create an account
+                    Enter your information to create an account<br></br>
+                    {mutation.isError && <span>Something Went Wrong</span>}
                 </CardDescription>
             </CardHeader>
             <CardContent>
                 <div className="grid gap-4">
                 <div className="grid gap-4">
-                    <Label htmlFor="first-name">Name</Label>
-                    <Input id="first-name" placeholder="Max" required />
+                    <Label htmlFor="name">Name</Label>
+                    <Input ref={nameRef} id="name" placeholder="Max" required />
                 </div>
                 <div className="grid gap-2">
                     <Label htmlFor="email">Email</Label>
                     <Input
+                    ref={emailRef}
                     id="email"
                     type="email"
                     placeholder="m@example.com"
@@ -31,10 +64,11 @@ const RegisterPage = () => {
                 </div>
                 <div className="grid gap-2">
                     <Label htmlFor="password">Password</Label>
-                    <Input id="password" type="password" />
+                    <Input ref={passwordRef} id="password" type="password" />
                 </div>
-                <Button type="submit" className="w-full">
-                    Create an account
+                <Button onClick={handleRegisterLogin} type="submit" className="w-full" disabled={mutation.isPending}>
+                    {mutation.isPending ? <Loader className='animate-spin' /> : '' }
+                    <span className='ml-2'> Create an account </span>
                 </Button>
                 </div>
                 <div className="mt-4 text-center text-sm">
